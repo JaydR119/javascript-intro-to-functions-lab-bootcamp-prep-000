dc2043ff49e21c3a293e60775cdbadb2fe59dd98
Jayd Robinson <jaydr119@gmail.com>
2019-08-07 19:16:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
-  } else {
-    return "YES INDEED!".toUpperCase()
   }
   if (string === 'I love you') {
     return 'I love you, too'
